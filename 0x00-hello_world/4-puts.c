e37b510f341574725938e4758b1ff9b57c0e2719
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-15 10:28:03 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	/**
+	 * print a line
+	 */
+	printf("Hello world, Welcome to African Tech Gurus\n");
+	return (0);
+}
