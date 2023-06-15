49ef924c1107d9ab150a24ca3c1581a2b436eae5
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-15 11:05:53 -0700

    Name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
-#include <stdio.h>
-
+#incude<stdio.h>
 /**
- * main - entry point
- *
- * Return:0
+ * main-prints the phrase "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 {
-	/**
-	 * print a line
-	 */
-	printf("Hello world, Welcome to African Tech Gurus\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
