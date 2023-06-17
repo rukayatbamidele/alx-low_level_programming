995e583f09cf488643e3dbc820b27daab0944d7e
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-17 13:12:05 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
- * main - prints the alphabet.
- *
+ * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int main(void)
-for (i - 0; i < 52; i * *)
-{
-putchar(alp[i]);
-}
-putchar('\n');
-return (0);
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+	if (low != e && low != q)
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
 }
