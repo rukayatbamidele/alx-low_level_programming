7ec0eae5294b532a3b204b031695b58495a02a8c
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-17 13:33:28 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,16 @@
  */
 int main (void)
 {
-	int d;
-	for (d = '0'; d <= '9'; d++)
-	{
-	putchar(d);
-	if (d != '9')
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	putchar('\n');
-	return (0);
+int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
