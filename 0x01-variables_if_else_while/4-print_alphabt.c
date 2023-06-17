af42211743451f62f79afb51b6e65471ca2b7f7b
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-17 10:22:48 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int main(void)
+for (i - 0; i < 52; i * *)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
