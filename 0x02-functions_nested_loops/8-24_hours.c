6794d62e729c8a92878f72dec3d67cc508e18170
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-20 13:24:08 -0700

    8-main.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of
- *              Jack Bauer, starting from 00:00 to 23:59.
- */
+*jack_bauer - Prints every minute of the day of
+*Jack Bauer, starting from 00:00 to 23:59.
+*/
 void jack_bauer(void)
 {
 int hour, minute;
-
-for (hour = 0; hour <= 23; hour++)
+for(hour = 0; hour <= 23; hour++)
 {
-for (minute = 0; minute <= 59; minute++)
+for(minute = 0; minute <= 59; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
@@ -18,7 +17,6 @@ _putchar(':');
 _putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
 _putchar('\n');
-                
 }
 }
 }
