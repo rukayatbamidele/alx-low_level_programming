fd8eea4d65dab8b920a500730f30b36a6582f827
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-20 12:02:58 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_HEADER_FILE
 
 int _putchar(char c);
-0x02-functions_nested_loops
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -13,4 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 #endif
