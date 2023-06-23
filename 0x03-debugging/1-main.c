6baea197da2da3db43abf5a63c681977e23bdc9a
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-23 08:40:39 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,11 +10,6 @@ int main(void)
 	int i;
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-	// Commented out the following section to avoid the infinite loop
-	// while (i < 10)
-	// {
-	// putchar(i);
-	// }
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
