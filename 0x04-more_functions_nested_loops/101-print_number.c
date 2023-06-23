54e0aa239b97d5fef9f2d5201f13f50712946782
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-22 20:17:06 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * print_number - print
- * @n: input
+ * Print_number - Print
+ * @n: Input
  * Return: Null
  */
 void print_number(int n)
 {
-int div = 1;
-int digit;
-if (n < 0)
-{
-n *= -1;
-_putchar('-');
-}
-while (n / div > 9)
-div *= 10;
-for (; div >= 1; div /= 10)
-{
-digit = n / div;
-n = n - (digit *div);
-_putchar('0' + digit);
-}
-}
+	int div = 1;
+	int digit;
+
+	if (n < 0)
+	{
+               n *= -1;
+	        _putchar('-');
+        }
+        while (n / div > 9)
+	       div *= 10;
+        for (; div >= 1; div /= 10)
+	{
+	     digit = n / div;
+             n = n - (digit * div);
+             _putchar('0' + digit);
+        }
+}	
+      
