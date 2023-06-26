0ace4a3c72245e1b178b1dc7c07c41724f59e526
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-26 16:04:23 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,26 +8,26 @@
 
 void puts_half(char *str)
 {
-	int j = 0;
-	int k;
+int j = 0;
+int k;
 
-	while (str[j] != '\0')
-	{
-	j++;
-	}
-	if (j % 2 == 1)
-	{
-	k = (j - 1) / 2;
-	k += 1;
-	}
-	else
-	{
-	k = j / 2;
-	}
+while (str[j] != '\0')
+{
+j++;
+}
+if (j % 2 == 1)
+{
+k = (j - 1) / 2;
+k += 1;
+}
+else
+{
+k = j / 2;
+}
 
-	for (; k < j; k++)
-	{
-	_putchar(str[k]);
-	}
-	_putchar('\n');
+for (; k < j; k++)
+{
+_putchar(str[k]);
+}
+_putchar('\n');
 }
