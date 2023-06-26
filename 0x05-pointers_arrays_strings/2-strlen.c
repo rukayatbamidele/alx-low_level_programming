9c27d6ed6a6688401be7e1cad9c174f6a8c34817
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-26 10:25:12 -0700

    2-starlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - Returns the lenght of a string.
  * @str: The string to get the lenght of 
- * Returns: The lenght of @str.
+ * Return: The lenght of @str.
  */
 size_t _strlen(const char *str)
 {
