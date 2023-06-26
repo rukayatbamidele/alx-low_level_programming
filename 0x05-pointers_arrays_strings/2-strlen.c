64d9059ac69eab2d4c2c2599c6fac704464f7a69
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-26 10:36:24 -0700

    2-starlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,12 +2,13 @@
 
 /**
  * _strlen - Returns the lenght of a string.
- * @str: The string to get the lenght of 
+ * @str: The string to get the lenght of
  * Return: The lenght of @str.
  */
 size_t _strlen(const char *str)
 {
 	size_t lenght = 0;
+
 	while (*str++)
 	lenght++;
 	return (lenght);
