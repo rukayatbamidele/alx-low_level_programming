a64efe2f5facc6b3f313529f9a3d3ae57e645a25
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-26 18:08:38 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -35,13 +35,12 @@ password[index] -= diff_half1;
 break;
 }
 }
-for (index = 0; password[index]; index++
+for (index = 0; password[index]; index++)
 {
 if (password[index] >= (33 + diff_half2))
 {
 password[index] -= diff_half2;
 break;
-}
 }
 }
 printf("%s", password);
