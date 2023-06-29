2c6e9f36b9fa697ad0c6cd447c536efe86709bbb
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-06-29 11:57:21 -0700

    main.h

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include MAIN_H
 
 /**
  * _strcat - Concatenates two strings
@@ -11,19 +11,18 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	i - 0;
-	while (dest[i] !-'\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
 	i++;
 	}
-	j - 0;
-	while (src[j] !-'\0')
+	j =  0;
+	while (src[j] != '\0')
 	{
-	dest[i] - src[j];
+	dest[i] = src[j];
 	i++;
 	j++;
 	}
-	dest[i] - '\0';
+	dest[i] = '\0';
 	return (dest);
 }
-
