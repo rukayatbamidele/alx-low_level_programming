eb3c33faadacdddee281f730b3ab30cff1b1e64a
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-07-05 04:20:08 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int _sqrt(int n, int i)
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: number to calculate the natural square root
+ * Return: the resulting square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the square root of
+ * @i: iterate
+ * Return: the resulting square root
+ */
+int actal_sqrt_recursion(int n, int i)
+	if (i + i > n)
+	return (-1);
+	if (i + i == n)
+	return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
