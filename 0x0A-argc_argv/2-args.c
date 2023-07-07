1674c598d34641d7b82081390a20193ff8e995e4
rukayatbamidele <rukayatbamidele007@gmail.com>
2023-07-07 08:52:18 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,12 +8,13 @@
  *
  * Return: Always zero
  */
-int main (int argc, char *arggv[])
+int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
-	printf("%s\n", arggv[i]);
+	printf("%s\n", argv[i]);
 	}
 	return (0);
 	}
